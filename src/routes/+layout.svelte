3ae6387ad9a3002633b31dddd7c83e63a7bffd32
weebified <77171064+weebified@users.noreply.github.com>
2023-08-29 21:58:11 -0400

    yes
    uwu

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<nav>
+	<div>
+		<a href="/">Home</a>
+		<a href="https://www.chiefdelphi.com/u/hyute/summary">Blog</a>
+		<a href="/Ressources">Ressources</a>
+		<a href="/Sponsors">Sponsors</a>
+		<a href="/Contact">Contact Us</a>
+	</div>
+</nav>
+
+<slot />
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
+	nav {
+        display: grid;
+        place-items: center;
+	}
+
+    a {
+        font-family: 'Poppins', sans-serif;
+        text-decoration: none;
+        font-weight: 550;
+        padding: 15px;
+        color: white;
+        opacity: 15%
+    }
+</style>
---
+++ src/routes/Contact/+page.svelte
@@ -0,0 +1,1 @@
+<div>weee</div>
