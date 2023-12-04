a4ec4e7af409cd15aee6c92cb8c68e57c295940e
weebified <77171064+weebified@users.noreply.github.com>
2023-12-03 20:56:21 -0500

    Document page finished

---
+++ src/routes/Ressources/Documents/+page.svelte
@@ -0,0 +1,116 @@
+<script>
+    import YearNav from '../../Components/YearNav.svelte';
+
+    let documentName = 'Lorem Ipsum';
+    let comment = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.';
+    let uploadDate = '2018-11-16';
+    let fileSize = '35kb'
+</script>
+
+<style>
+    main{
+        margin-top: 100px;
+        display: grid;
+        place-content: center;
+    }
+
+    #mainContainer{
+        width: 90vw;
+        height: calc(100vh - 200px);
+        display: grid;
+        grid-template-columns: 20vw 50vw 20vw;
+    }
+
+    .title{
+        margin-bottom: 30px;
+        color: #ffcc01;
+        font-size: 120%;
+    }
+
+    #itemList{
+        text-decoration: none;
+        height: 55vh;
+    }
+
+    li{
+        list-style-type: none;
+        opacity: 20%;
+        cursor: pointer;
+        transition: 100ms;
+    }
+
+    li:hover{
+        opacity: 30%;
+    }
+
+    #yearNav{
+        position: relative;
+    }
+
+    #yearNavContainer{
+        height: 10vh;
+        position: absolute;
+        bottom: 0;
+    }
+
+    #file{
+        width: 100%;
+        height: 100%;
+        display: grid;
+        place-content: center;
+    }
+
+    #file p {
+        opacity: 30%;
+    }
+
+    #docInfo{
+        padding-left: 55px;
+    }
+
+    #comment, #metaData, #download{
+        opacity: 20%;
+        margin-bottom: 15px;
+        transition: 100ms;
+    }
+
+    #download{
+        cursor: pointer;
+    }
+
+    #download:hover{
+        opacity: 30%;
+    }
+</style>
+
+<main>
+    <section id="mainContainer">
+        <section id="navBarContainer">
+            <p class="title">Documents</p>
+            <ul id="itemList">
+                <li>Folder name</li>
+                <li>Folder name</li>
+                <li>Folder name</li>
+                <li>Folder name</li>
+                <li>Folder name</li>
+            </ul>
+            <section id="yearNavContainer">
+                <div id="yearNav">
+                    <YearNav/>
+                </div>
+            </section>
+        </section>
+        <section id="file">
+            <p>No file is open</p>
+        </section>
+        <section id="docInfo">
+            <p class="title">{documentName}</p>
+            <p id="comment">Comment: {comment}</p>
+            <div id="metaData">
+                <p id="fileSize">Size: {fileSize}</p>
+                <p id="uploadDate">Upload date: {uploadDate}</p>
+            </div>
+            <a id="download">Download</a>
+        </section>
+    </section>
+</main>
--- src/routes/Ressources/Photos/+page.svelte
+++ src/routes/Ressources/Photos/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     import Carousel from '../../Components/Carousel.svelte';
+    import YearNav from '../../Components/YearNav.svelte';
 
-    let yearPeriod = '2018 - 2019';
     let numberPhotos = '49'
 </script>
 
@@ -21,14 +21,10 @@
         display: flex;
         justify-content: space-between;
         width: 90vw;
-        margin-left: calc(10vw / 2);
+        margin-left: 5vw;
         font-size: 110%;
     }
 
-    #yearNav{
-        display: flex;
-        gap: 3px;
-    }
 </style>
 
 <main>
@@ -51,15 +47,7 @@
 		/>
         <section id="bottomNavContainer">
             <section id="bottomNav">
-                <section id="yearNav">
-                    <svg width="20" height="20" viewBox="0 -1.5 11 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M0.646447 3.64645C0.451184 3.84171 0.451184 4.15829 0.646447 4.35355L3.82843 7.53553C4.02369 7.7308 4.34027 7.7308 4.53553 7.53553C4.7308 7.34027 4.7308 7.02369 4.53553 6.82843L1.70711 4L4.53553 1.17157C4.7308 0.976311 4.7308 0.659728 4.53553 0.464466C4.34027 0.269204 4.02369 0.269204 3.82843 0.464466L0.646447 3.64645ZM10 4.5C10.2761 4.5 10.5 4.27614 10.5 4C10.5 3.72386 10.2761 3.5 10 3.5V4.5ZM1 4.5H10V3.5H1V4.5Z" fill="white"/>
-                    </svg>
-                    <p id="yearPeriod">{yearPeriod}</p>
-                    <svg width="20" height="20" viewBox="0 -1.5 11 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M1 3.5C0.723858 3.5 0.5 3.72386 0.5 4C0.5 4.27614 0.723858 4.5 1 4.5V3.5ZM10.3536 4.35355C10.5488 4.15829 10.5488 3.84171 10.3536 3.64645L7.17157 0.464466C6.97631 0.269204 6.65973 0.269204 6.46447 0.464466C6.2692 0.659728 6.2692 0.976311 6.46447 1.17157L9.29289 4L6.46447 6.82843C6.2692 7.02369 6.2692 7.34027 6.46447 7.53553C6.65973 7.7308 6.97631 7.7308 7.17157 7.53553L10.3536 4.35355ZM1 4.5H10V3.5H1V4.5Z" fill="white"/>
-                    </svg>                    
-                </section>
+                <YearNav/>
                 <p id="numberPhotos">{numberPhotos} Photos</p>
                 <p>Photo Gallery</p>
             </section>        
